b4bbe0a1df8e2b78ea16a955bb101ed5378e29ba
daudi1kule <daudikule949@gmail.com>
2021-11-22 20:43:47 +0300

    thrird commit. Assignment complete

--- DetermineTriangle.java
+++ DetermineTriangle.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 
+
 public class DetermineTriangle {
     // call main method
     public static void main(String[] args) {
@@ -14,15 +15,34 @@ public class DetermineTriangle {
         // c
         String c = JOptionPane.showInputDialog("Enter size for c", 1);
         System.out.println("c is : " + c+ " \n");
+        int x = Integer.parseInt(a);
+        int y = Integer.parseInt(b);
+        int z = Integer.parseInt(c);
         // b = args[1];
         // c = args[2];
-        String[] triangle =  [a, b, c]
+        // String[] triangle =  [a, b, c]
+        // switch to int values limit to 100
+        String msg = null;
+        if(x ==y && y == z) {
+            // return triangle is equilateral - 3 sides
+            msg = "All sides are equal. Triangle is Equilateral! \n";
+            System.out.println(msg);
+        } else if (x == y || y==z || x==z) {
+            // two sides will be equal
+            // get diff in the 2 sides
+            msg = "Only two sides are equal. Triangle is isosceles! \n";
+            System.out.println(msg);
+        } else {
+            // triangle is scalene
+            msg = "No sides are equal. Triangle is Scalene! \n";
+            System.out.println(msg);
+        }
 
         // determine length of sides
         // if all sides = equal then is a equilateral
         // if 2 sides = equal then is isosceles
         // if no equal sides = equal then triangle is scalene
-
+        System.out.println("End of File execution");
 
     }
 
